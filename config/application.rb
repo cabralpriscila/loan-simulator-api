6310afe4a2fa12344fe03e8367454347f1127a4a
cabralpriscila <priscilacabral.dev@gmail.com>
2024-11-19 17:50:11 -0300

    chore: utils folder

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module LoanSimulatorApi
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.autoload_paths << Rails.root.join('app/utils')
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
