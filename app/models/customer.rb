d68aeb4aa0a388d4495ab5c41fa35fcaf38a3a42
cabralpriscila <priscilacabral.dev@gmail.com>
2024-11-19 10:51:23 -0300

    fix: cpf for document_number

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :document_number, presence: true, cpf: true
+  validates :document_number, presence: true, document_number: true
   validates :phone, format: { with: /\A\(\d{2}\)\s\d{4,5}-\d{4}\z/ }, allow_blank: true
   validates :zip_code, format: { with: /\A\d{5}-\d{3}\z/ }, allow_blank: true
   validates :birthdate, presence: true
