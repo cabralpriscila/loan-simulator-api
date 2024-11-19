8c94bce52efc85535035b9fdb33923e006261394
cabralpriscila <priscilacabral.dev@gmail.com>
2024-11-19 11:13:13 -0300

    chore: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_19_122919) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_19_140655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_19_122919) do
     t.decimal "income", precision: 10, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_customers_on_deleted_at"
     t.index ["document_number"], name: "index_customers_on_document_number", unique: true
   end
 end
