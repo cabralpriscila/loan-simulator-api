a58a7fcffa3e523852960d2204985323aeb4c7ac
cabralpriscila <priscilacabral.dev@gmail.com>
2024-11-19 17:40:00 -0300

    feat: update routes and update loan simulator controller

--- app/controllers/api/v1/loan_simulators_controller.rb
+++ app/controllers/api/v1/loan_simulators_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class LoanSimulatorsController < ApplicationController
+      before_action :set_loan_simulator, only: [ :show, :update, :destroy ]
+
       def index
         @loan_simulators = LoanSimulator.page(params[:page]).per(params[:per_page] || 10)
 
@@ -12,8 +14,48 @@ module Api
             prev_page: @loan_simulators.prev_page,
             total_pages: @loan_simulators.total_pages,
             total_count: @loan_simulators.total_count
-          }
-        }, status: :ok
+          }, status: :ok
+        }
+      end
+
+      def show
+        render json: @loan_simulator
+      end
+
+      def create
+        @loan_simulator = LoanSimulator.new(loan_simulator_params)
+
+        if @loan_simulator.save
+          LoanCalculationService.call(@loan_simulator)
+          render json: @loan_simulator, status: :created
+        else
+          render json: { errors: @loan_simulator.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @loan_simulator.update(loan_simulator_params)
+          render json: @loan_simulator
+        else
+          render json: { errors: @loan_simulator.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @loan_simulator.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_loan_simulator
+        @loan_simulator = LoanSimulator.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Simulador de crédito não encontrado" }, status: :not_found
+      end
+
+      def loan_simulator_params
+        params.require(:loan_simulator).permit(:customer_id, :requested_amount, :term_in_months)
       end
     end
   end
