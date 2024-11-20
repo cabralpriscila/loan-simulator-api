2c60753ca7d823b01827b631e60f759a5eea22b4
cabralpriscila <priscilacabral.dev@gmail.com>
2024-11-20 16:16:25 -0300

    feat: update route with update status

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers
-      resources :loan_simulators
+      resources :loan_simulators do
+        member do
+          patch :update_status
+        end
+      end
     end
   end
 end
