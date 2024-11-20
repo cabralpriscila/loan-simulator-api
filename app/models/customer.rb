d50a1f75514d84c7c36c53ed0a180ddebb01cc88
cabralpriscila <priscilacabral.dev@gmail.com>
2024-11-20 20:32:08 -0300

    refactor: update document number to be unique

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :document_number, presence: true, document_number: true
+  validates :document_number, presence: true, uniqueness: true, document_number: true
   validates :phone, format: { with: /\A\(\d{2}\)\s\d{4,5}-\d{4}\z/ }, allow_blank: true
   validates :zip_code, format: { with: /\A\d{5}-\d{3}\z/ }, allow_blank: true
   validates :birthdate, presence: true
